adf1da0f20fe0d6260b6d1cf575aa53b8c264ef4
aima-habyarimana <117313916+aima-habyarimana@users.noreply.github.com>
2022-11-14 21:05:21 +0000

    Add files via upload

---
+++ SciPy.py
@@ -0,0 +1,113 @@
+'''
+# Basics of SciPy
+
+#import scipy
+#print(scipy.__version__)
+
+from scipy import constants
+
+print(constants.liter)
+print(constants.pi)
+print(dir(constants))  # A list of all units under the constants module
+
+# Meter
+
+from scipy import constants
+
+print(constants.yotta)    #1e+24
+print(constants.zetta)    #1e+21
+print(constants.exa)      #1e+18
+print(constants.peta)     #1000000000000000.0
+print(constants.tera)     #1000000000000.0
+print(constants.giga)     #1000000000.0
+print(constants.mega)     #1000000.0
+print(constants.kilo)     #1000.0
+print(constants.hecto)    #100.0
+print(constants.deka)     #10.0
+print(constants.deci)     #0.1
+print(constants.centi)    #0.01
+print(constants.milli)    #0.001
+print(constants.micro)    #1e-06
+print(constants.nano)     #1e-09
+print(constants.pico)     #1e-12
+print(constants.femto)    #1e-15
+print(constants.atto)     #1e-18
+print(constants.zepto)    #1e-21
+
+# Bytes
+
+from scipy import constants
+
+print(constants.kibi)    #1024
+print(constants.mebi)    #1048576
+print(constants.gibi)    #1073741824
+print(constants.tebi)    #1099511627776
+print(constants.pebi)    #1125899906842624
+print(constants.exbi)    #1152921504606846976
+print(constants.zebi)    #1180591620717411303424
+print(constants.yobi)    #1208925819614629174706176
+
+# Mass
+
+from scipy import constants
+
+print(constants.gram)        #0.001
+print(constants.metric_ton)  #1000.0
+print(constants.grain)       #6.479891e-05
+print(constants.lb)          #0.45359236999999997
+print(constants.pound)       #0.45359236999999997
+print(constants.oz)          #0.028349523124999998
+print(constants.ounce)       #0.028349523124999998
+print(constants.stone)       #6.3502931799999995
+print(constants.long_ton)    #1016.0469088
+print(constants.short_ton)   #907.1847399999999
+print(constants.troy_ounce)  #0.031103476799999998
+print(constants.troy_pound)  #0.37324172159999996
+print(constants.carat)       #0.0002
+print(constants.atomic_mass) #1.66053904e-27
+print(constants.m_u)         #1.66053904e-27
+print(constants.u)           #1.66053904e-27
+
+
+# Time
+from scipy import constants
+
+print(constants.minute)      #60.0
+print(constants.hour)        #3600.0
+print(constants.day)         #86400.0
+print(constants.week)        #604800.0
+print(constants.year)        #31536000.0
+print(constants.Julian_year) #31557600.0
+
+# Length
+
+from scipy import constants
+
+print(constants.inch)              #0.0254
+print(constants.foot)              #0.30479999999999996
+print(constants.yard)              #0.9143999999999999
+print(constants.mile)              #1609.3439999999998
+print(constants.mil)               #2.5399999999999997e-05
+print(constants.pt)                #0.00035277777777777776
+print(constants.point)             #0.00035277777777777776
+print(constants.survey_foot)       #0.3048006096012192
+print(constants.survey_mile)       #1609.3472186944373
+print(constants.nautical_mile)     #1852.0
+print(constants.fermi)             #1e-15
+print(constants.angstrom)          #1e-10
+print(constants.micron)            #1e-06
+print(constants.au)                #149597870691.0
+print(constants.astronomical_unit) #149597870691.0
+print(constants.light_year)        #9460730472580800.0
+print(constants.parsec)            #3.0856775813057292e+16
+
+
+# Power
+
+from scipy import constants
+
+print(constants.hp)         #745.6998715822701
+print(constants.horsepower) #745.6998715822701
+
+'''
+
